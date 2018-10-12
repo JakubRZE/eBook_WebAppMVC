26c0fa487da1aa6a380e3e87610a969e7a567faa
Jakub Rzepka <Jakubjakk@gmail.com>
2018-10-13 00:51:45 +0200

    Innitial Commit

---
+++ EbookWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EbookWebApp.MvcApplication" Language="C#" %>
---
+++ EbookWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EbookWebApp.Startup))]
+namespace EbookWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
